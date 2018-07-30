ec4eeb21b353acb622f9a29ffa366b51c9751683
zhangxin <zhangxin@gridsum.com>
2018-07-30 11:02:35 +0800

    init proj && add template

---
+++ BufferExcelWriter/BufferExcelWriter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BufferExcelWriter
+{
+    public class BufferExcelWriter
+    {
+        public String Config { get; set; }
+    }
+}
