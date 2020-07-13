106772f9d26c26bd7aeca626a2d2e81bc3cf7ee6
itoktsnhc <itoktsnhc@hotmail.com>
2020-07-13 16:19:18 +0800

    obslete slow async version

--- BufferExcelWriter.Sample/Program.cs
+++ BufferExcelWriter.Sample/Program.cs
@@ -9,53 +9,17 @@ namespace BufferExcelWriter.Sample
 {
     public static class Program
     {
-        private static async Task Main()
+        private static void Main()
         {
-            await WriteTestDataAsyncSample0();
-            await WriteTestDataAsyncSample1();
+            WriteTestDataSample1();
+            WriteTestDataSampleAsync().Wait();
+            Console.ReadLine();
         }
 
-        private static async Task WriteTestDataAsyncSample0()
+        private static void WriteTestDataSample1()
         {
-            var header = new RowDfn
-            {
-                Cells = new List<CellDfn>
-                {
-                    new CellDfn("Name"),
-                    new CellDfn("Val"),
-                    new CellDfn("noVal") //no value cell 
-                }
-            };
-            //add headers
-            var sheet = new WorkSheetDfn("Odd", header);
-
-            sheet.BufferedRows.Add(new RowDfn
-            {
-                Cells = new List<CellDfn>
-                {
-                    new CellDfn("Name", "Hello"),
-                    new CellDfn("Val", "World")
-                }
-            });
-
-            //output
-            using (var wb = new WorkBookDfn("tempFolder"))
-            {
-                wb.Sheets.Add(sheet);
-                await wb.FlushBufferedRowsAsync(true);
-                using (var fs = File.Create($"{DateTime.Now.Ticks}.xlsx"))
-                {
-                    using (var stream = wb.BuildExcelAndGetStreamAsync().Result)
-                    {
-                        stream.Position = 0;
-                        stream.CopyTo(fs);
-                    }
-                }
-            }
-        }
-
-        private static async Task WriteTestDataAsyncSample1()
-        {
+            var sw = new Stopwatch();
+            sw.Start();
             var header = new RowDfn
             {
                 Cells = new List<CellDfn>
@@ -73,13 +37,13 @@ namespace BufferExcelWriter.Sample
             wb.Sheets.Add(evenSheet);
 
 
-            var sw = new Stopwatch();
+
             foreach (var outerIndex in Enumerable.Range(0, 10))
             {
                 sw.Reset();
                 var size = 10000;
-                var strad = (char) 0xb;
-                var inValidStr = new string(new[] {strad});
+                var strad = (char)0xb;
+                var inValidStr = new string(new[] { strad });
                 foreach (var index in Enumerable.Range(outerIndex * size, size))
                 {
                     if (index % 2 == 0)
@@ -105,23 +69,16 @@ namespace BufferExcelWriter.Sample
                         });
                     }
                 }
-
-
-                sw.Start();
-                await wb.FlushBufferedRowsAsync(true); //flushDataAndClean
-                sw.Stop();
-                Console.WriteLine(sw.Elapsed);
+                wb.FlushBufferedRows(true); //flushDataAndClean
             }
 
             var insertSheet = new WorkSheetDfn("aaa", header);
             wb.Sheets.Add(insertSheet);
             evenSheet.Header.Cells.Add(new CellDfn("Latter1"));
             oddSheet.Header.Cells.Add(new CellDfn("Latter2"));
-            sw.Reset();
-            sw.Start();
             using (var fs = File.Create($"{DateTime.Now.Ticks}.xlsx"))
             {
-                using (var stream = wb.BuildExcelAndGetStreamAsync().Result)
+                using (var stream = wb.BuildExcelAndGetStream())
                 {
                     stream.Position = 0;
                     stream.CopyTo(fs);
@@ -130,9 +87,77 @@ namespace BufferExcelWriter.Sample
 
             wb.Dispose();
             sw.Stop();
-            Console.WriteLine($"build and clean cost : {sw.Elapsed}");
-            Console.WriteLine("Over");
-            Console.ReadLine();
+            Console.WriteLine($"WriteTestDataSample1 cost : {sw.Elapsed}");
+        }
+
+        private static async Task WriteTestDataSampleAsync()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            var header = new RowDfn
+            {
+                Cells = new List<CellDfn>
+                {
+                    new CellDfn("Nam®e"),
+                    new CellDfn("Index"),
+                    new CellDfn("noVal")
+                }
+            };
+            var oddSheet = new WorkSheetDfn("Odd", header); //add headers
+            var evenSheet = new WorkSheetDfn("Even", header);
+
+            var wb = new WorkBookDfn("tempFolder");
+            wb.Sheets.Add(oddSheet);
+            wb.Sheets.Add(evenSheet);
+            foreach (var outerIndex in Enumerable.Range(0, 10))
+            {
+                var size = 10000;
+                var strad = (char)0xb;
+                var inValidStr = new string(new[] { strad });
+                foreach (var index in Enumerable.Range(outerIndex * size, size))
+                {
+                    if (index % 2 == 0)
+                    {
+                        evenSheet.BufferedRows.Add(new RowDfn
+                        {
+                            Cells = new List<CellDfn>
+                            {
+                                new CellDfn("Name", inValidStr),
+                                new CellDfn("Index", index.ToString())
+                            }
+                        });
+                    }
+                    else
+                    {
+                        oddSheet.BufferedRows.Add(new RowDfn
+                        {
+                            Cells = new List<CellDfn>
+                            {
+                                new CellDfn("Nam®e", $"f￥￥￥©$ \"oo{index}"),
+                                new CellDfn("Index", index.ToString())
+                            }
+                        });
+                    }
+                }
+                await wb.FlushBufferedRowsAsync(true); //flushDataAndClean
+            }
+
+            var insertSheet = new WorkSheetDfn("aaa", header);
+            wb.Sheets.Add(insertSheet);
+            evenSheet.Header.Cells.Add(new CellDfn("Latter1"));
+            oddSheet.Header.Cells.Add(new CellDfn("Latter2"));
+            using (var fs = File.Create($"{DateTime.Now.Ticks}.xlsx"))
+            {
+                using (var stream = wb.BuildExcelAndGetStream())
+                {
+                    stream.Position = 0;
+                    stream.CopyTo(fs);
+                }
+            }
+
+            wb.Dispose();
+            sw.Stop();
+            Console.WriteLine($"WriteTestDataSampleAsync cost : {sw.Elapsed}");
         }
     }
 }
--- BufferExcelWriter/WorkBookDfn.cs
+++ BufferExcelWriter/WorkBookDfn.cs
@@ -103,6 +103,8 @@ namespace BufferExcelWriter
             }
         }
 
+        #region  Obsleted Async Version
+        [Obsolete]
         private async Task UpdateSheetRelationshipAsync()
         {
             InitFromZipFile();
@@ -267,24 +269,11 @@ namespace BufferExcelWriter
             }
         }
 
-        private void UpdateSheetNum()
-        {
-            for (var i = 0; i < Sheets.Count; i++)
-            {
-                var currentSheet = Sheets[i];
-                if (string.IsNullOrWhiteSpace(currentSheet.Name))
-                {
-                    currentSheet.Name = $"Sheet{i + 1}";
-                }
-
-                currentSheet.SheetNum = i + 1;
-            }
-        }
-
         /// <summary>
         ///     Flush bufferedRow data to disk and clean buffered row
         /// </summary>
         /// <returns></returns>
+        [Obsolete]
         public async Task FlushBufferedRowsAsync(bool needGc = false)
         {
             UpdateSheetNum();
@@ -317,10 +306,10 @@ namespace BufferExcelWriter
                 GC.Collect();
             }
         }
-
         /// <summary>
         ///     write end info to file and get file stream back
         /// </summary>
+        [Obsolete]
         public async Task<Stream> BuildExcelAndGetStreamAsync(bool needGc = true)
         {
             if (Sheets.Count == 0)
@@ -347,6 +336,7 @@ namespace BufferExcelWriter
             return _outputStream;
         }
 
+        [Obsolete]
         private async Task GenerateSheetFileAsync(WorkSheetDfn sheet)
         {
             var sheetEntry = FolderEntry.GetEntry(sheet.GetEntryName());
@@ -384,6 +374,282 @@ namespace BufferExcelWriter
                 await sheet.SheetStreamWriter.FlushAsync();
             }
         }
+        #endregion
+
+        #region SyncVersion
+
+        public void FlushBufferedRows(bool needGc = false)
+        {
+            UpdateSheetNum();
+            foreach (var sheet in Sheets)
+            {
+                var tempDataEntry = TempFolderEntry.CreateEntry(string.Format(TempBodyFileName, sheet.SheetNum));
+                if (sheet.TempDataStreamWriter == null)
+                {
+                    sheet.TempDataStream = tempDataEntry.Open();
+                    sheet.TempDataStreamWriter = new StreamWriter(sheet.TempDataStream);
+                }
+
+                var sheetEntry = FolderEntry.GetEntry(sheet.GetEntryName());
+                if (sheetEntry == null)
+                {
+                    throw new Exception("Init needed!!");
+                }
+
+                foreach (var currentRow in sheet.BufferedRows)
+                {
+                    sheet.TempDataStreamWriter.WriteLine(JsonConvert.SerializeObject(currentRow));
+                }
+
+                sheet.TempDataStreamWriter.Flush();
+                sheet.BufferedRows.Clear();
+            }
+
+            if (needGc)
+            {
+                GC.Collect();
+            }
+        }
+        private void UpdateSheetNum()
+        {
+            for (var i = 0; i < Sheets.Count; i++)
+            {
+                var currentSheet = Sheets[i];
+                if (string.IsNullOrWhiteSpace(currentSheet.Name))
+                {
+                    currentSheet.Name = $"Sheet{i + 1}";
+                }
+
+                currentSheet.SheetNum = i + 1;
+            }
+        }
+        private void UpdateSheetRelationship()
+        {
+            InitFromZipFile();
+            UpdateSheetNum();
+
+            foreach (var sheet in Sheets)
+            {
+                #region Update [Content_Types].xml
+
+                var contentTypeEntry = FolderEntry.GetEntry("[Content_Types].xml");
+                if (contentTypeEntry == null)
+                {
+                    throw new FileNotFoundException("[Content_Types].xml");
+                }
+
+                using (var contentTypeStream = contentTypeEntry.Open())
+                {
+                    using (var sr = new StreamReader(contentTypeStream))
+                    {
+                        var doc = new XmlDocument();
+                        doc.LoadXml(sr.ReadToEnd());
+                        var types = doc.GetElementsByTagName("Types")
+                            .Cast<XmlNode>()
+                            .First();
+                        if (doc.DocumentElement != null)
+                        {
+                            var element = doc.CreateElement("Override", doc.DocumentElement.NamespaceURI);
+                            element.SetAttribute("PartName", $"/xl/worksheets/sheet{sheet.SheetNum}.xml");
+                            element.SetAttribute("ContentType",
+                                "application/vnd.openxmlformats-officedocument.spreadsheetml.worksheet+xml");
+                            types.AppendChild(element);
+                        }
+
+                        contentTypeStream.Position = 0;
+                        contentTypeStream.SetLength(0);
+                        doc.Save(contentTypeStream);
+                    }
+                }
+
+                #endregion
+
+
+                #region Update xl/_rels/workbook.xml.rels
+
+                var identifier = "rId";
+                var relsEntry = FolderEntry.GetEntry("xl/_rels/workbook.xml.rels");
+                if (relsEntry == null)
+                {
+                    throw new FileNotFoundException("xl/_rels/workbook.xml.rels");
+                }
+
+                using (var relsStream = relsEntry.Open())
+                {
+                    using (var sr = new StreamReader(relsStream))
+                    {
+                        var result = sr.ReadToEnd();
+                        var doc = new XmlDocument();
+                        doc.LoadXml(result);
+                        var relationships = doc.GetElementsByTagName("Relationships")
+                            .Cast<XmlNode>()
+                            .First();
+                        identifier += (relationships.ChildNodes.Count + 1).ToString();
+                        if (doc.DocumentElement != null)
+                        {
+                            var element = doc.CreateElement("Relationship", doc.DocumentElement.NamespaceURI);
+                            element.SetAttribute("Target", $"worksheets/sheet{sheet.SheetNum}.xml");
+                            element.SetAttribute("Type",
+                                "http://schemas.openxmlformats.org/officeDocument/2006/relationships/worksheet");
+                            element.SetAttribute("Id", identifier);
+                            relationships.AppendChild(element);
+                        }
+
+                        relsStream.Position = 0;
+                        relsStream.SetLength(0);
+                        doc.Save(relsStream);
+                    }
+                }
+
+                #endregion
+
+
+                #region Update xl/workbook.xml
+
+                var workbookEntry = FolderEntry.GetEntry("xl/workbook.xml");
+                if (workbookEntry == null)
+                {
+                    throw new FileNotFoundException("xl/workbook.xml");
+                }
+
+                using (var workbookStream = workbookEntry.Open())
+                {
+                    using (var sr = new StreamReader(workbookStream))
+                    {
+                        var doc = new XmlDocument();
+                        doc.LoadXml(sr.ReadToEnd());
+                        var tags = doc.GetElementsByTagName("sheets")
+                            .Cast<XmlNode>()
+                            .First();
+                        if (doc.DocumentElement != null)
+                        {
+                            var element = doc.CreateElement("sheet", doc.DocumentElement.NamespaceURI);
+                            tags.AppendChild(element);
+                            var attr = doc.CreateAttribute("r", "id",
+                                "http://schemas.openxmlformats.org/officeDocument/2006/relationships");
+                            attr.Value = identifier;
+                            element.Attributes.Append(attr);
+                            element.SetAttribute("sheetId", sheet.SheetNum.ToString());
+                            element.SetAttribute("name", sheet.Name);
+                        }
+
+                        workbookStream.Position = 0;
+                        workbookStream.SetLength(0);
+                        doc.Save(workbookStream);
+                    }
+                }
+
+                #endregion
+
+
+                #region Update docProps/app.xml
+
+                var appEntry = FolderEntry.GetEntry("docProps/app.xml");
+                if (appEntry == null)
+                {
+                    throw new FileNotFoundException("docProps/app.xml Not Found in FolderEntry");
+                }
+
+                using (var appStream = appEntry.Open())
+                {
+                    using (var sr = new StreamReader(appStream))
+                    {
+                        var result = sr.ReadToEnd();
+                        var xd = new XmlDocument();
+                        xd.LoadXml(result);
+                        var element = xd.CreateElement("vt:lpstr",
+                            "http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes");
+                        element.InnerText = sheet.Name;
+                        var tmpEle = xd
+                            .GetElementsByTagName("vt:vector")
+                            .Cast<XmlNode>()
+                            .Single(x => x.Attributes != null && x.Attributes["baseType"].Value == "lpstr");
+                        tmpEle.AppendChild(element);
+
+                        if (tmpEle.Attributes != null)
+                        {
+                            tmpEle.Attributes["size"].Value =
+                                (Convert.ToInt32(tmpEle.Attributes["size"].Value) + 1).ToString();
+                        }
+
+                        var tmp2 = xd.GetElementsByTagName("vt:i4")
+                            .Cast<XmlNode>()
+                            .Single();
+                        var val = string.IsNullOrWhiteSpace(tmp2.InnerText) ? 1 : Convert.ToInt32(tmp2.InnerText) + 1;
+                        tmp2.InnerText = val.ToString();
+                        appStream.Position = 0;
+                        appStream.SetLength(0);
+                        xd.Save(appStream);
+                    }
+                }
+
+                #endregion
+            }
+        }
+        public Stream BuildExcelAndGetStream(bool needGc = true)
+        {
+            if (Sheets.Count == 0)
+            {
+                throw new InvalidOperationException("no sheet in workbook");
+            }
+
+            FlushBufferedRows(needGc);
+            UpdateSheetRelationship();
+
+            foreach (var sheet in Sheets)
+            {
+                GenerateSheetFile(sheet);
+            }
+
+            Directory.Delete(_tempFolder, true);
+            ZipFile.CreateFromDirectory(_workingFolder, _outPutFilePath, CompressionLevel.Optimal, false);
+            if (Directory.Exists(_workingFolder))
+            {
+                Directory.Delete(_workingFolder, true);
+            }
+
+            _outputStream = File.Open(_outPutFilePath, FileMode.Open);
+            return _outputStream;
+        }
+        private void GenerateSheetFile(WorkSheetDfn sheet)
+        {
+            var sheetEntry = FolderEntry.GetEntry(sheet.GetEntryName());
+            if (sheetEntry == null)
+            {
+                throw new FileNotFoundException(sheet.GetEntryName());
+            }
+
+            sheet.SheetFileStream = sheetEntry.Open();
+            using (sheet.SheetStreamWriter = new StreamWriter(sheet.SheetFileStream))
+            {
+                sheet.SheetStreamWriter.Write(WorksheetDefaultHeaders);
+                var rowNumber = 1;
+                sheet.SheetStreamWriter.Write(sheet.Header.ToXmlString(rowNumber, sheet.Header,
+                    sheet.NullValStr));
+                if (sheet.TempDataStream != null)
+                {
+                    sheet.TempDataStream.Seek(0, SeekOrigin.Begin);
+                    using (var streamReader = new StreamReader(sheet.TempDataStream))
+                    {
+                        while (!streamReader.EndOfStream)
+                        {
+                            rowNumber++;
+                            var line = streamReader.ReadLine();
+                            var currentRow = JsonConvert.DeserializeObject<RowDfn>(line);
+                            sheet.SheetStreamWriter.Write(currentRow.ToXmlString(rowNumber,
+                                sheet.Header,
+                                sheet.NullValStr));
+                        }
+                    }
+                }
+
+                sheet.SheetStreamWriter.Write(SheetDataDefaultFooter);
+                sheet.SheetStreamWriter.Write(WorksheetDefaultFooter);
+                sheet.SheetStreamWriter.Flush();
+            }
+        }
+
+        #endregion
     }
 
     internal class FolderEntry
